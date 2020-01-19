fa23f017d8aba961af55ad6c2522449ee441782d
IzabelaGA <izabela.sosnowska23@gmail.com>
2020-01-20 00:06:06 +0100

    We can now add tasks:)

--- app.js
+++ app.js
@@ -4,3 +4,46 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+// Load Event Listeners
+loadEventListeners();
+
+// Load All Event Listeners
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener('submit', addTask);
+}
+
+// Add Task
+function addTask(e) {
+  if (taskInput.value === '') {
+    alert('Add a task');
+  }
+
+  // Create li element
+  const li = document.createElement('li');
+  // Add clas
+  li.className = 'collection-item';
+  // Create text node and append to the li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  // Add class
+  link.className = 'delete-item secondary-content';
+  // Add icon Html
+  link.innerHTML = '<i class="fa fa-remove"></i>'
+  // Append the link to li
+  li.appendChild(link);
+
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Clear input
+  taskInput.value = '';
+
+
+  console.log(li);
+
+
+  e.preventDefault();
+}
