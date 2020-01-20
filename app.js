0ff31341ac537f44cbc275547f44451025ee610b
IzabelaGA <izabela.sosnowska23@gmail.com>
2020-01-20 22:46:54 +0100

    We can remove task

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
 }
 
 // Add Task
@@ -46,4 +48,13 @@ function addTask(e) {
 
 
   e.preventDefault();
+}
+
+// Remove task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are You Sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
 }
