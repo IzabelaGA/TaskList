e558b2cac79a093037a43bec3cd6500d8ec3461e
IzabelaGA <izabela.sosnowska23@gmail.com>
2020-01-21 22:08:43 +0100

    Set tasks in localStorage

--- app.js
+++ app.js
@@ -44,14 +44,25 @@ function addTask(e) {
   // Append li to ul
   taskList.appendChild(li);
 
+  // Store in LS
+  storeTaskInLocalStorage(taskInput.value);
+
   // Clear input
   taskInput.value = '';
 
-
-  console.log(li);
-
-
   e.preventDefault();
+}
+
+// Store Task
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // Remove task
