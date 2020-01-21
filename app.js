f9b2194ad3e58a2b08163faa592f142ac0097054
IzabelaGA <izabela.sosnowska23@gmail.com>
2020-01-21 22:20:41 +0100

    Show tasks after reload (without possib. clear for ever yet)

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ loadEventListeners();
 
 // Load All Event Listeners
 function loadEventListeners() {
+  // DOM load event
+  document.addEventListener('DOMContentLoaded', getTasks);
   // Add task event
   form.addEventListener('submit', addTask);
   // Remove task event
@@ -18,6 +20,35 @@ function loadEventListeners() {
   clearBtn.addEventListener('click', clearTasks);
   // Filter task event
   filter.addEventListener('keyup', filterTasks);
+}
+
+// Get Tasks from LS
+function getTasks() {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  tasks.forEach(function(task){
+// Create li element
+const li = document.createElement('li');
+// Add clas
+li.className = 'collection-item';
+// Create text node and append to the li
+li.appendChild(document.createTextNode(task));
+// Create new link element
+const link = document.createElement('a');
+// Add class
+link.className = 'delete-item secondary-content';
+// Add icon Html
+link.innerHTML = '<i class="fa fa-remove"></i>'
+// Append the link to li
+li.appendChild(link);
+
+// Append li to ul
+taskList.appendChild(li);
+  })
 }
 
 // Add Task
